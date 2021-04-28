a1caafa3444b027b2c9475bd3bd554f495500974
Matt Yang <matt.yang2012@gmail.com>
2021-04-28 14:54:43 +0800

    add reportstate and request sync

--- washer-start/functions/index.js
+++ washer-start/functions/index.js
@@ -254,9 +254,18 @@ exports.smarthome = functions.https.onRequest(app);
 exports.requestsync = functions.https.onRequest(async (request, response) => {
   response.set('Access-Control-Allow-Origin', '*');
   functions.logger.info(`Request SYNC for user ${USER_ID}`);
-
-  // TODO: Call HomeGraph API for user '123'
-  response.status(500).send(`Request SYNC not implemented`);
+  try {
+    const res = await homegraph.devices.requestSync({
+      requestBody: {
+        agentUserId: USER_ID,
+      },
+    });
+    functions.logger.info('Request sync response:', res.status, res.data);
+    response.json(res.data);
+  } catch (err) {
+    functions.logger.error(err);
+    response.status(500).send(`Error requesting sync: ${err}`);
+  }
 });
 
 /**
@@ -266,7 +275,27 @@ exports.requestsync = functions.https.onRequest(async (request, response) => {
 exports.reportstate = functions.database.ref('{deviceId}').onWrite(
     async (change, context) => {
       functions.logger.info('Firebase write event triggered Report State');
+      const snapshot = change.after.val();
 
-      // TODO: Get latest state and call HomeGraph API
+      const requestBody = {
+        requestId: 'ff36a3cc', /* Any unique ID */
+        agentUserId: USER_ID,
+        payload: {
+          devices: {
+            states: {
+              /* Report the current state of our washer */
+              [context.params.deviceId]: {
+                on: snapshot.OnOff.on,
+                isPaused: snapshot.StartStop.isPaused,
+                isRunning: snapshot.StartStop.isRunning,
+              },
+            },
+          },
+        },
+      };
+
+      const res = await homegraph.devices.reportStateAndNotification({
+        requestBody,
+      });
+      functions.logger.info('Report state response:', res.status, res.data);
     });
-
